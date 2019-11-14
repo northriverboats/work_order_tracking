068b311a5343db6e67cfce4357c16ab497508218
Fredrick W. Warren <fredw@northriverboats.com>
2019-11-14 11:22:22 -0800

    moved values to .env and created systemd rq worker

--- backend/workorderapi/config.py
+++ backend/workorderapi/config.py
@@ -3,12 +3,19 @@ config.py
 - settings for the flask application object
 """
 import os
+from distutils.util import strtobool
+
+
+def envrion_tf(key):
+    temp = strtobool(os.environ.get(key))
+    return True if temp == 1 else False
 
 
 class BaseConfig(object):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///workorder.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = envrion_tf('DEBUG')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = envrion_tf(
+        'SQLALCHEMY_TRACK_MODIFICATIONS')
+    REDIS_URL = os.environ.get('REDIS_URL')
     # used for encryption and session management
-    SECRET_KEY = 'mysecretkeybooger!!!'
-    REDIS_URL = os.environ.get('REDIS_URL') or 'redis://'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
--- backend/workorderapi/db.py
+++ backend/workorderapi/db.py
@@ -7,6 +7,6 @@ from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
-engine = create_engine(os.environ['SQLALCHEMY_URL'])
+engine = create_engine(os.environ['SQLALCHEMY_DATABASE_URI'])
 
 Session = scoped_session(sessionmaker(bind=engine))
